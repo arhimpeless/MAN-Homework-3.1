8162c670cef71d38fa243731dc23b81390766ad8
Andrey Markov <arherower@outlook.com>
2022-09-14 17:50:39 +0300

    14,09

--- Program.cs
+++ Program.cs
@@ -4,15 +4,16 @@
     {
         static void Main(string[] args)
         {
-            //Method_1();
-            //Method_2();
-            //Method_3();
+            Method_1();
+            Method_2();
+            Method_3();
             int min = 1;
             int max = 2;
             Method_4 (min, max); 
         }
         public static void Method_4(int min, int max)
         {
+            Console.WriteLine("\nВывод целых чисел по заданному диапазону.");
             Console.Write("Введите целое значение диапазона от: ");
             min = int.Parse(Console.ReadLine());
             Console.Write("Введите целое значение диапазона до: ");
@@ -29,47 +30,61 @@
             {
                 do
                 {
+                    Console.Write("Выполнение условия: ");
                     Console.Write(random.Next(min, max));
+                    break;
                 }
-                while (myInt == min || myInt > max);
-                Console.ReadKey();
+                while (myInt == min || myInt < max);
+                Console.Write(" ");
+                Console.Write(random.Next(min, max));
             }
+            Console.ReadKey();
         }
         private static void Method_3()
         {
             int k;
+            Console.WriteLine("Вывод целых положительных чисел кратных 3-м и 5-и до числа, которое укажет пользователь.");
             Console.Write("Введите целое положительное число: ");
             k = int.Parse(Console.ReadLine());
             if (k > 0)
             {
-                for (int i = 0; i < k; i++)    //&& i %= 3 && i = 5
+                Console.Write("Выполнение условия: ");
+                for (int i = 0; i < k; i++)   
                 {
-                    if (i % 3 == 0 && i % 5 == 0)
+                    if (i %3 == 0 && i %5 == 0)
                     {
-                        Console.WriteLine(i);
+                        Console.Write(" " + i);
+                        Thread.Sleep(20);
                     }
+                    continue;
                 }
             }
-            Console.WriteLine("Операция завершена!");
+            Console.WriteLine("\n Операция завершена!");
+            return;
         }
         private static void Method_2()
         {
-            Console.WriteLine("Числа от 10 до 1:");
+            Console.WriteLine("Вывести на консоль числа от 10 до 1:");
             int j = 11;
             while (j > 1)
             {
                 j--;
-                Console.WriteLine(j);
+                Console.Write(" " + j);
+                Thread.Sleep(120);
             }
+            Console.WriteLine("\n");
+            return;
         }
         private static void Method_1()
         {
-            Console.WriteLine("Числа от 0 до 9:");
+            Console.WriteLine("Вывести на консоль числа от 0 до 9:");
             for (int i = 0; i < 10; i++)
             {
-                Console.WriteLine(i);
+                Console.Write(" " + i);
+                Thread.Sleep(120);
             }
-            Console.ReadKey();
+            Console.WriteLine("\n");
+            return;
         }
     }
 }
